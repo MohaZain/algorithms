94884389591ed2050cdec76aeae4f34bceab5950
mohazain <gw.zain@gmail.com>
2021-08-31 14:01:30 +0300

    fix binary_oct

--- binary_oct.py
+++ binary_oct.py
@@ -11,19 +11,27 @@ def binary_oct(bainry):
     print(decim)
     oct_indx = 1
     octi = ''
+    oct_indx_zeros = 0
     while (decim != 0):
       x = int(decim / oct_indx)
       if x == 0: 
         octi = octi + str(decim)
         decim = 0
-        break
       if  x <= 7:
+        # print(x,oct_indx,decim)
         octi = octi + str(x)
         decim =  decim - oct_indx * x
+        print(x,oct_indx,decim)
+        if decim == 0:
+            oct_indx_zeros = oct_indx
         oct_indx = 1
       oct_indx = oct_indx * 8
+    while(oct_indx_zeros !=1 ):
+        octi = octi + '0'
+        print(oct_indx_zeros)
+        oct_indx_zeros = int(oct_indx_zeros / 8)
     return octi
-bainry = '10011'
+bainry = '1000000000'
 # 25
 x = binary_oct(bainry)
 print(x)
